01fce1c36ad612154d2c011a0a2d08793a7d4e1a
Vritant Bhardwaj <vritoku@gmail.com>
2024-07-22 11:10:20 -0700

    add numpy

---
+++ site-packages/numpy/version.py
@@ -0,0 +1,8 @@
+
+version = "1.26.0b1"
+__version__ = version
+full_version = version
+
+git_revision = "7bc18034031f32e5d03bb646c472dabd1623e9d5"
+release = 'dev' not in version and '+' not in version
+short_version = version.split("+")[0]
